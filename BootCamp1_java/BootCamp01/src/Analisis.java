23fcd9d66e9f9a017ec7b468d1128ba2c75e0510
Bernardo Vázquez Vanegas <BVAZQUEZV@invex.com>
2026-03-30 18:58:15 -0600

    Actatualizacion avance

---
+++ BootCamp1_java/BootCamp01/src/Analisis.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Analisis
+{     public static void main(String[] args) {// Punto A
+    int x = 10;                              // Punto B
+         String nombre = "Java";                  // Punto C
+    int[] arr = {1, 2, 3};                  // Punto D
+    procesarDatos(nombre, arr);
+}     static void procesarDatos(String n, int[] a) { // Punto E
+            int temporal = a.length;                 // Punto F
+     }                                            // Punto G
+}
