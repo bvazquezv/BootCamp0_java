0fe8087e14fab2ffaf75af1a8642959cd7602421
Your Name <you@example.com>
2026-04-07 12:57:17 -0600

    docs :  Actualizacion 07/04/2026

--- BootCamp1_java/demo/untitled/src/CodigoVivo.java
+++ BootCamp1_java/demo/untitled/src/CodigoVivo.java
@@ -5,11 +5,11 @@ public static void main(String[] args) {
     String c = "Java";
     String d = c.intern();
     System.out.println(a == b); //true
-    // toma del String pool y compara siendo la misma referencai dado que no se instancio el objeto
+    // toma del String pool y compara siendo la misma referencia dado que no se instancio el objeto
     System.out.println(a == c);  // true
-    // toma del String pool y compara siendo la misma referencai dado que no se instancio el objeto
+    // toma del String pool y compara siendo la misma referencia dado que no se instancio el objeto
     System.out.println(a == d);  //  true
-    //// d se crea de un literal existene en el String pool y apunta a la mima cadena de alli que resulta o mismo
+    //// d se crea de un literal existente (intern) del String pool y apunta a la mima cadena de allí que resulta o mismo
     System.out.println(a.equals(c));
     //Compara el contenido del objeto pese a que aparentemente se comparan los objetos se esta comparando el contenido
     System.out.println("ss");
@@ -18,8 +18,9 @@ public static void main(String[] args) {
     Integer p = 128;
     Integer q = 128;
     System.out.println(x == y);  //true
-    //java crea un cache de objetos integer de -127 a 128
+    //java crea un cache de objetos integer de -128 a 127
     System.out.println(p == q);
+    //Aqui no hay cahce de bjetos por lo que se gnera un objeto di
     //
 }
 
